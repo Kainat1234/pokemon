c8c2c4fb3e6a5ca188bbb3c0cf625d435a09eb0d
Kainat1234 <iqbalkainatt.com>
2025-04-16 03:34:04 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// import './App.css'
+
+import { Pokemon } from "./component/Pokemon"
+
+export const App = () => {
+  
+  return (
+    <>
+   <Pokemon/>
+    </>
+  )
+}
+
+// export default App
